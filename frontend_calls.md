c8d1ffd2341fd831ab8666ff5c4d16f6cd673a8e
carobuck <cbuck717@gmail.com>
2022-03-22 12:21:05 -0700

    first commit

---
+++ frontend_calls.md
@@ -0,0 +1,24 @@
+## How to call the backend
+And what comes back.
+
+### For all calls, need to pass:
+* TBD
+* format of parameters: {"country":"Germany","startingEvent":"startEvent_1","interventions":[25,25,25],"request":"viz_line"}
+
+## Route XXX
+Will return ... TBD
+### Call:
+curl -H 'Content-Type: application/json' -X POST -d '{ params-dict }' app-engine-link
+### Returns:
+<details>
+<summary>Click to expand!</summary>
+
+Lots of stuff can be hidden here
+</details>
+
+## Route XXX
+Will return ... TBD
+### Call:
+curl -H 'Content-Type: application/json' -X POST -d '{ params-dict }' app-engine-link
+### Returns:
+{"num_infected":20993}
---
+++ main.py
@@ -0,0 +1,67 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+import pandas as pd
+from datetime import date
+import json
+
+app = Flask(__name__)
+CORS(app)
+
+# Initialize creds for reading data from firestore
+cred = credentials.Certificate('firebase_secret.json')
+firebase_admin.initialize_app(cred)
+db = firestore.client()
+
+## FUNCTIONS
+def wrangle_cal(input):
+    df = pd.DataFrame(input,columns = ['value'])
+    df['day'] = pd.date_range(start=date.today(), periods=len(df), freq='D').strftime('%Y-%m-%d')
+    df_dict = df.to_dict(orient = 'records') # use to_dict() to get into format Nivo needs
+    return df_dict
+
+    data['data'] = wrangle_cal(data['data'])
+## APP ROUTES
+@app.route("/")
+def homepage():
+    return 'Home Page Route flask gcp app'
+
+@app.route('/about')
+def about():
+    return 'About Page Route V1'
+
+
+## ALT IF WE SAVE LONGER-DOC/COLLECTION CHAIN IN FIRESTORE ##
+@app.route("/getData", methods=["POST"])
+def getData():
+    if request.method == "POST":
+        data = request.get_json()
+
+        # Get all user input from front end
+        country = data['country']
+        starting_event = data['startingEvent']
+        vax,sd,quar = data['interventions'] # interventions passed as list of #: [vax,socialDist,quar]
+        interventions = 'vax' + str(vax) + '_sd' + str(sd) + '_quar' + str(quar) # put in format to match in firestore
+
+        data_req = data['request'] # what piece of info is frontend asking for? (e.g. map, line chart data, etc.)
+
+        # Pull collection/document for specified parameters:
+        doc_ref = db.collection(country).document(interventions).collection(starting_event).document(data_req)
+        doc = doc_ref.get()
+        data = doc.to_dict()
+
+        if data_req == 'viz_cal': # Need to add dynamic dates to daily infection values
+            data['data'] = wrangle_cal(data['data'])
+
+        if data_req == 'viz_map': # Need to convert 'load' data to convert to json (from string format firestore needs to save)
+            data['data'] = json.loads(data['data'])
+
+        return jsonify(data)
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
